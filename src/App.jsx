b11ab8891e3ca7f5942b87767046d1b22d33adf1
SharonSebastian32 <sharonsebastian81@gmail.com>
2024-10-24 17:30:28 +0530

    feature/addnumber to password

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ function App() {
 
   const copyPasswordToclipboard = useCallback(() => {
     passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
     window.navigator.clipboard.writeText(password);
   }, [password]);
 
